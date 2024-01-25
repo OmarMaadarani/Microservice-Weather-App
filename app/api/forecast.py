77cc80b68c7c0389409320c71f6692536eb73a55
Omar Maadarani <omaadarani@outlook.com>
2024-01-25 13:34:01 -0500

    README Update, as well as setup App and API.
    Still in work the works, now if you run the app and go to  docs url, you should see the main GET request, which gets hourly forecast of current day & on current time by default.

---
+++ app/api/forecast.py
@@ -0,0 +1,36 @@
+from typing import Union
+from fastapi import Header, APIRouter
+import requests
+from datetime import datetime
+
+WEATHER_API_KEY = "03d772011e1b4b46bd2201058242401"
+base_url = "http://api.weatherapi.com/v1/forecast.json?key=" + WEATHER_API_KEY
+#&q=Ottawa&days=1&aqi=no&alerts=no
+
+forecast = APIRouter()
+
+def get_forecast_body(forecast_res: dict):
+	return forecast_res["forecast"]["forecastday"][0]
+
+
+def get_hourly(city_forecast: dict, start_time: int):
+	hourly = city_forecast["hour"]
+	if start_time == 0:
+		return hourly
+	return hourly[start_time:]
+
+@forecast.get("/")
+async def index(start_now: bool = True):
+	city = "Ottawa"
+	req_url = base_url + f"&q={city}&days=1&aqi=no&alerts=no"
+	res = requests.get(req_url).json()
+	
+	forecastday = get_forecast_body(res)
+	# Default start-time to 0 (Index of 12am/0 Hour in hour array)
+	start = 0
+	if start_now == True:
+		today = datetime.now()
+		start = int(today.strftime("%H"))
+	
+	hour_forecast = get_hourly(forecastday, start)
+	return hour_forecast
