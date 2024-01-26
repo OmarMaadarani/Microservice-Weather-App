8219f5217f28f8f8cd99904cb86307545cc4cf0f
Omar Maadarani <omaadarani@outlook.com>
2024-01-26 14:53:28 -0500

    Added new Forecast Endpoints & Schemas + forecast.py: +  - Added new Weekly, Weekly Detailed, & Three-Day forecast endpoints. "Index" re-named to "Hourly" + - Made the Weekly Forecast as the default/index endpoint instead of Hourly Forecast
    + models.py:
    + - Created 3 new Models/Schemas to parse WeatherAPI Response.
    + - Day Schema: Parses the response data for the average weather conditions for the day
    + - Forecast Schema: Describes the Forecast Date along with its weather condtions using the "Day" model
    + - ForecastDetailed Schema: Child Schema of Forecast, adding the hourly forecast of the date, using a List of "Hour" Model/Schema

--- app/api/forecast/models.py
+++ app/api/forecast/models.py
@@ -2,8 +2,20 @@ from pydantic import BaseModel
 from typing import List
 
 
+class Day(BaseModel):
+    """ Class Describing Necessary Data Needed for the Forecast of a Day from WeatherAPI """
+    avgtemp_c: float
+    condition: dict
+    avghumidity: int
+    daily_chance_of_rain: int
+    daily_chance_of_snow: int
+    totalprecip_mm: float
+    totalsnow_cm: float    
+    uv: int
+
+
 class Hour(BaseModel):
-    """ Class Describing Necessary Data Needed from Hourly Forecast from WeatherAPI """
+    """ Class Describing Necessary Data Needed for Hourly Forecast from WeatherAPI """
     time: str
     temp_c: float
     is_day: int
@@ -18,4 +30,13 @@ class Hour(BaseModel):
     will_it_snow: int
     chance_of_snow: int
     uv: int
+
+class Forecast(BaseModel):
+    """ Class Describing Base Forecast Data to tie "Day" Model to it's Date from WeatherAPI """
+    date: str
+    day: Day
+
+class ForecastDetailed(Forecast):
+    """ Sub-Class of the Forecast Class/Model, adding the Hourly Forecast Data for the Date from WeatherAPI """
+    hour: List[Hour]
 
